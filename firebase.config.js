a06329c26ad6f6f649960edbbda0001138b9b31e
Florian Junior <florjunior3012@gmail.com>
2023-10-09 19:25:18 -0300

    adicionando variaveis de ambiente

--- firebase.config.js
+++ firebase.config.js
@@ -2,17 +2,23 @@ import { initializeApp } from 'firebase/app';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { initializeAuth, getReactNativePersistence  } from 'firebase/auth';
 
+//TODO usar react native notenv para lidar com variaveis locais
 const firebaseConfig = {
-    apiKey: 'AIzaSyDgv41PqN-mUs0VxYtf8P7n2fJdnZgx44k',
-    authDomain: 'bibliotech-db.firebaseapp.com',
-    databaseURL: 'https://bibliotech-db-default-rtdb.firebaseio.com',
-    projectId: 'bibliotech-db',
-    storageBucket: 'bibliotech-db.appspot.com',
-    messagingSenderId: '382278025928',
-    appId: '1:382278025928:web:72b90e0092e30565d15bfd'
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DB_URL,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 };
 
-const app = initializeApp(firebaseConfig);
+let app = null;
+
+if (!app) {
+    console.log(process.env)
+    app = initializeApp(firebaseConfig);
+}
 
 initializeAuth(app, {
     persistence: getReactNativePersistence(ReactNativeAsyncStorage)
